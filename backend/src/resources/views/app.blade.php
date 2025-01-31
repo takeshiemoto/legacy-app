d8f49606670a36c5b1dca86f380466b0d37a47c2
takeshiemoto <private.takeshiemoto@gmail.com>
2025-01-31 10:37:28 +0900

    chore: update HTML structure in app.blade.php and modify package-lock.json dependencies

--- backend/src/resources/views/app.blade.php
+++ backend/src/resources/views/app.blade.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
